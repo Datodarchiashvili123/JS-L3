617fe62be5ac881035b60b869bce19a7bca215cd
Davidson1 <datodarchiashvili@gmail.com>
2021-10-14 19:50:09 +0400

    L3

---
+++ index.js
@@ -0,0 +1,64 @@
+const people = [
+    {
+      name: 'Ben',
+      age: 30,
+      occupation: 'Banker',
+      salary: 3000,
+      pets: []
+    },
+    {
+      name: 'Jane',
+      age: 26,
+      occupation: 'Teacher',
+      salary: 1200,
+      pets: ['dog']
+    },
+    {
+      name: 'John',
+      age: 31,
+      occupation: 'Developer',
+      salary: 4000,
+      pets: ['dog', 'cat']
+    },
+    {
+      name: 'Mike',
+      age: 26,
+      occupation: 'Developer',
+      salary: 5000,
+      pets: []
+    },
+    {
+      name: 'Tom',
+      age: 34,
+      occupation: 'Teacher',
+      salary: 3400,
+      pets: ['dog', 'parrot']
+    }
+  ];
+
+// 1. Calculate sum of all teacher salaries
+let sumOfSalary = 0;
+people.forEach(people => sumOfSalary += people.salary);
+console.log('sum of salary = ', sumOfSalary);
+
+// 2. Create a new array with the same people but double salaries of developers
+let doubleSalary;
+doubleSalary = people.map(people => people.salary * 2 );
+console.log('doubleSalary : ', doubleSalary);
+
+// 3. Filter people who has a dog
+let dogOwner = people.filter(people => people.pets.includes('dog'));
+console.log('dogOwner : ', dogOwner);
+
+// 4. Figure out if all the people have pets or not
+let havePetsorNot = people.every(people => people.pets === '');
+console.log('have pets or not : ', havePetsorNot);
+
+// 5. Figure out if any of the people are above age 30
+
+let isAnyoneAbove30 = people.some(people => people.age > 30);
+console.log('is anyone above 30: ', isAnyoneAbove30);
+
+///---///
+
+//მეტი ვერ მოვასწარი დენი არ მქონდა :დდდ
